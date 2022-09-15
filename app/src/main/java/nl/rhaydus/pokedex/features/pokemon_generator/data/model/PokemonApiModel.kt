59e78a94abdd62ea00fa8b243c28634f6745ed0d
CinqueIzumi <nofeednolife@gmail.com>
2022-09-15 16:29:13 +0200

    Pokemon model now holds main type

--- app/src/main/java/nl/rhaydus/pokedex/features/pokemon_generator/data/model/PokemonApiModel.kt
+++ app/src/main/java/nl/rhaydus/pokedex/features/pokemon_generator/data/model/PokemonApiModel.kt
@@ -5,10 +5,19 @@ import com.google.gson.annotations.SerializedName
 data class PokemonApiModel(
     val name: String,
     val id: Int,
-    val sprites: PokemonSpritesApiModel
+    val sprites: PokemonSpritesApiModel,
+    val types: List<PokemonTypeEntryApiModel>
 )
 
 data class PokemonSpritesApiModel(
     @SerializedName("front_default")
     val url: String,
+)
+
+data class PokemonTypeEntryApiModel(
+    val slot: Int,
+    val type: PokemonTypeApiModel
+)
+data class PokemonTypeApiModel(
+    val name: String
 )
--- app/src/main/java/nl/rhaydus/pokedex/features/pokemon_generator/domain/model/Pokemon.kt
+++ app/src/main/java/nl/rhaydus/pokedex/features/pokemon_generator/domain/model/Pokemon.kt
@@ -4,4 +4,5 @@ data class Pokemon(
     val name: String,
     val id: Int,
     val imageUrl: String,
+    val mainType: String,
 )
