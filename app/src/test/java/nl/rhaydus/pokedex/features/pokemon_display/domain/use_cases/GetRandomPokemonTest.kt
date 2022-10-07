959357cc6838dad7fdd9782dbb0896e90f4aa224
CinqueIzumi <nofeednolife@gmail.com>
2022-10-07 10:56:06 +0200

    Add domain layer tests

---
+++ app/src/test/java/nl/rhaydus/pokedex/features/pokemon_display/domain/use_cases/GetRandomPokemonTest.kt
@@ -0,0 +1,39 @@
+package nl.rhaydus.pokedex.features.pokemon_display.domain.use_cases
+
+import io.kotest.matchers.shouldBe
+import io.mockk.clearAllMocks
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
+import nl.rhaydus.pokedex.features.pokemon_display.domain.model.Pokemon
+import nl.rhaydus.pokedex.features.pokemon_display.domain.repositories.PokemonRepository
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class GetRandomPokemonTest {
+    private val mockPokemonRepository: PokemonRepository = mockk()
+    private val mockPokemon: Pokemon = mockk()
+
+    private val useCase: GetRandomPokemon = GetRandomPokemon(mockPokemonRepository)
+
+    @BeforeEach
+    fun init() {
+        clearAllMocks()
+    }
+
+    @Test
+    fun `calls repository for data`() {
+        // ----- Arrange -----
+        coEvery {
+            mockPokemonRepository.getRandomPokemon()
+        }.returns(Result.success(mockPokemon))
+
+        // ----- Act -----
+        val result = runBlocking { useCase() }
+
+        // ----- Assert -----
+        result shouldBe Result.success(mockPokemon)
+    }
+}
