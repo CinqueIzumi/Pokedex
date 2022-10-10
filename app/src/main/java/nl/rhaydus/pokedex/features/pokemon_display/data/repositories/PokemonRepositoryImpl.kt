432684fb4cb27072c27602e0d87808b84a36f5be
CinqueIzumi <nofeednolife@gmail.com>
2022-10-10 16:07:37 +0200

    Implement UnFavoritePokemon repository

--- app/src/main/java/nl/rhaydus/pokedex/features/pokemon_display/data/repositories/PokemonRepositoryImpl.kt
+++ app/src/main/java/nl/rhaydus/pokedex/features/pokemon_display/data/repositories/PokemonRepositoryImpl.kt
@@ -33,7 +33,6 @@ class PokemonRepositoryImpl(
     override suspend fun favoritePokemon(pokemon: Pokemon): Result<Boolean> =
         runCatching { localPokemonDataSource.favoritePokemon(pokemon) }
 
-    override suspend fun unFavoritePokemon(pokemon: Pokemon): Result<Boolean> {
-        TODO("Not yet implemented")
-    }
+    override suspend fun unFavoritePokemon(pokemon: Pokemon): Result<Boolean> =
+        runCatching { localPokemonDataSource.unFavoritePokemon(pokemon) }
 }
