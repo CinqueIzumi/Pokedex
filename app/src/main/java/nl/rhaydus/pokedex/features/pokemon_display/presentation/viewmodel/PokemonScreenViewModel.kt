79a92c05432c0c7f87134bbf7220070dad84a1b0
CinqueIzumi <nofeednolife@gmail.com>
2022-10-12 09:37:24 +0200

    Implement GetPokemonWithFilter functionality

--- app/src/main/java/nl/rhaydus/pokedex/features/pokemon_display/presentation/viewmodel/PokemonScreenViewModel.kt
+++ app/src/main/java/nl/rhaydus/pokedex/features/pokemon_display/presentation/viewmodel/PokemonScreenViewModel.kt
@@ -3,22 +3,21 @@ package nl.rhaydus.pokedex.features.pokemon_display.presentation.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.sqlite.db.SimpleSQLiteQuery
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import nl.rhaydus.pokedex.features.pokemon_display.data.dao.PokemonDao
 import nl.rhaydus.pokedex.features.pokemon_display.data.mapper.toPokemon
-import nl.rhaydus.pokedex.features.pokemon_display.data.mapper.toPokemonList
 import nl.rhaydus.pokedex.features.pokemon_display.data.model.PokemonEntity
 import nl.rhaydus.pokedex.features.pokemon_display.domain.model.Pokemon
+import nl.rhaydus.pokedex.features.pokemon_display.domain.use_cases.GetPokemonWithFilter
 import timber.log.Timber
 import javax.inject.Inject
 import kotlin.Exception
 
 @HiltViewModel
 class PokemonScreenViewModel @Inject constructor(
-    private val pokeDao: PokemonDao,
+    private val pokeDao: PokemonDao, private val getPokemonWithFilterUseCase: GetPokemonWithFilter
 ) : ViewModel() {
 
     private val _currentPokemonList = MutableLiveData<List<Pokemon>?>()
@@ -36,85 +35,13 @@ class PokemonScreenViewModel @Inject constructor(
         withContext(Dispatchers.IO) {
             _loadingState.postValue(true)
 
-            try {
-                var queryString = ""
-                val args: MutableList<Any> = mutableListOf()
-                var containsConditions = false
+            val result = getPokemonWithFilterUseCase(givenQuery, favorites, mainType, secondaryType)
 
-                // Start the assembly of the query string
-                queryString += "SELECT * FROM pokemonentity"
-
-                // Add the search filter if required
-                givenQuery?.let { givenQuery ->
-                    // Check whether the given query is numerical (id) or a pokemon name
-                    when (givenQuery.toIntOrNull()) {
-                        null -> {
-                            queryString += " WHERE poke_name LIKE ?"
-                            args.add("%$givenQuery%")
-                        }
-                        else -> {
-                            queryString += " WHERE id = ?"
-                            args.add(givenQuery)
-                        }
-                    }
-
-                    containsConditions = true
-                }
-
-                // Add the favorites filter if required
-                favorites?.let { fav ->
-                    if (containsConditions) {
-                        queryString += " AND"
-                    } else {
-                        queryString += " WHERE"
-                        containsConditions = true
-                    }
-                    queryString += " favorite = ?"
-                    args.add(fav)
-                }
-
-                // Add the main type filter if required
-                mainType?.let { mainType ->
-                    if (mainType != "All") {
-                        if (containsConditions) {
-                            queryString += " AND"
-                        } else {
-                            queryString += " WHERE"
-                            containsConditions = true
-                        }
-                        queryString += " poke_main_type = ?"
-                        args.add(mainType)
-                    }
-                }
-
-                // Add the secondary type filter if required
-                secondaryType?.let { secondaryType ->
-                    if (secondaryType != "All") {
-                        if (containsConditions) {
-                            queryString += " AND"
-                        } else {
-                            queryString += " WHERE"
-                            containsConditions = true
-                        }
-                        queryString += " poke_secondary_type = ?"
-                        args.add(secondaryType)
-                    }
-                }
-
-                // End of the query
-                queryString += ";"
-
-                Timber.d(queryString)
-
-                // Perform the query
-                val query = SimpleSQLiteQuery(queryString, args.toList().toTypedArray())
-                val pokeList: List<Pokemon> = pokeDao.getFilteredPokemons(query).toPokemonList()
-
+            result.fold(onSuccess = { pokeList ->
                 _currentPokemonList.postValue(pokeList)
-            } catch (e: Exception) {
-                Timber.e(e)
-            }
-
+            }, onFailure = { error ->
+                Timber.e(error)
+            })
             _loadingState.postValue(false)
         }
     }
