1ab83910fa2388e55310b547ccb4a384f146f09f
Cinque <ba.peereboom@gmail.com>
2024-01-16 21:49:41 +0100

    Add Parameterized test dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -132,10 +132,11 @@ dependencies {
     implementation("com.jakewharton.timber:timber:5.0.1")
 
     // Unit tests dependencies
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
     testImplementation("io.kotest:kotest-assertions-core:5.4.2")
     testImplementation("io.mockk:mockk:1.13.7")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.0")
 }
 
 tasks.withType<Test> {
