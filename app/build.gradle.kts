422f25f62eda7f2fd50ad9babfca25276d075c52
Cinque <ba.peereboom@gmail.com>
2024-01-16 17:12:37 +0100

    Add unit testing dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -132,9 +132,9 @@ dependencies {
     implementation("com.jakewharton.timber:timber:5.0.1")
 
     // Unit tests dependencies
-    testImplementation("androidx.test:core-ktx:1.5.0")
-    testImplementation("androidx.test.ext:junit-ktx:1.1.5")
-    testImplementation("org.robolectric:robolectric:4.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    testImplementation("io.kotest:kotest-assertions-core:5.4.2")
     testImplementation("io.mockk:mockk:1.13.7")
 }
 
