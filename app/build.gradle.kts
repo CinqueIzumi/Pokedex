241b89ed5790b58752c1abdd37418844fdd655f8
Cinque <ba.peereboom@gmail.com>
2025-01-21 08:03:37 +0100

    Implement retrofit logging

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -121,6 +121,7 @@ dependencies {
     val retrofitVersion = "2.9.0"
     implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
     implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
     // Firebase
     implementation(platform("com.google.firebase:firebase-bom:26.1.1"))
--- app/src/main/java/nl/rhaydus/pokedex/core/data/di/RetrofitModule.kt
+++ app/src/main/java/nl/rhaydus/pokedex/core/data/di/RetrofitModule.kt
@@ -4,8 +4,12 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import nl.rhaydus.pokedex.BuildConfig
 import nl.rhaydus.pokedex.features.pokemon_display.data.network.PokemonApiService
 import nl.rhaydus.pokedex.features.splash.data.network.SplashScreenApiService
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -15,18 +19,43 @@ import javax.inject.Singleton
 object RetrofitModule {
     @Provides
     @Singleton
-    fun getRetrofit(): Retrofit = Retrofit.Builder()
+    fun getLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            level = if (BuildConfig.BUILD_TYPE != "release") {
+                HttpLoggingInterceptor.Level.BODY
+            } else {
+                HttpLoggingInterceptor.Level.NONE
+            }
+        }
+    }
+
+    @Provides
+    @Singleton
+    fun getClient(
+        loggingInterceptor: HttpLoggingInterceptor
+    ): OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    @Provides
+    @Singleton
+    fun getRetrofit(
+        client: OkHttpClient
+    ): Retrofit = Retrofit.Builder()
+        .client(client)
         .baseUrl("https://pokeapi.co/api/v2/")
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
     @Provides
     @Singleton
-    fun getPokemonApiService(retrofit: Retrofit): PokemonApiService =
-        retrofit.create(PokemonApiService::class.java)
+    fun getPokemonApiService(
+        retrofit: Retrofit,
+    ): PokemonApiService = retrofit.create(PokemonApiService::class.java)
 
     @Provides
     @Singleton
-    fun getSplashScreenApiService(retrofit: Retrofit): SplashScreenApiService =
-        retrofit.create(SplashScreenApiService::class.java)
+    fun getSplashScreenApiService(
+        retrofit: Retrofit,
+    ): SplashScreenApiService = retrofit.create(SplashScreenApiService::class.java)
 }
